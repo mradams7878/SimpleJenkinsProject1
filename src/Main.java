132948000ec46425a287c77ef489dde36c862a63
mradams7878 <draeadams78@gmail.comgit config --global user.email draeadams78@gmail.com>
2026-01-07 19:13:18 -0500

    Changed to 15

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 15; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("Hello World");
